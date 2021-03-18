3e44a53ee8c76d712db18f6421faacd8b0e20b4f
Usuario <Usuario@LAPTOP-453FQ6JN>
2021-03-18 11:45:08 +0100

    javadoc clases

---
+++ src/test/java/cuentas/CCuentaTest.java
@@ -0,0 +1,188 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuentas;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Mariluz
+ */
+public class CCuentaTest {
+    
+    public CCuentaTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of estado method, of class CCuenta.
+     */
+    @Test
+    public void testEstado() {
+        System.out.println("estado");
+        CCuenta instance = new CCuenta();
+        double expResult = 0.0;
+        double result = instance.estado();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of ingresar method, of class CCuenta.
+     */
+    @Test
+    public void testIngresar() throws Exception {
+        System.out.println("ingresar");
+        double cantidad = 0.0;
+        CCuenta instance = new CCuenta();
+        instance.ingresar(cantidad);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of retirar method, of class CCuenta.
+     */
+    @Test
+    public void testRetirar() throws Exception {
+        System.out.println("retirar");
+        double cantidad = 0.0;
+        CCuenta instance = new CCuenta();
+        instance.retirar(cantidad);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getNombre method, of class CCuenta.
+     */
+    @Test
+    public void testGetNombre() {
+        System.out.println("getNombre");
+        CCuenta instance = new CCuenta();
+        String expResult = "";
+        String result = instance.getNombre();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNombre method, of class CCuenta.
+     */
+    @Test
+    public void testSetNombre() {
+        System.out.println("setNombre");
+        String nombre = "";
+        CCuenta instance = new CCuenta();
+        instance.setNombre(nombre);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCuenta method, of class CCuenta.
+     */
+    @Test
+    public void testGetCuenta() {
+        System.out.println("getCuenta");
+        CCuenta instance = new CCuenta();
+        String expResult = "";
+        String result = instance.getCuenta();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCuenta method, of class CCuenta.
+     */
+    @Test
+    public void testSetCuenta() {
+        System.out.println("setCuenta");
+        String cuenta = "";
+        CCuenta instance = new CCuenta();
+        instance.setCuenta(cuenta);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSaldo method, of class CCuenta.
+     */
+    @Test
+    public void testGetSaldo() {
+        System.out.println("getSaldo");
+        CCuenta instance = new CCuenta();
+        double expResult = 0.0;
+        double result = instance.getSaldo();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSaldo method, of class CCuenta.
+     */
+    @Test
+    public void testSetSaldo() {
+        System.out.println("setSaldo");
+        double saldo = 0.0;
+        CCuenta instance = new CCuenta();
+        instance.setSaldo(saldo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTipoInterés method, of class CCuenta.
+     */
+    @Test
+    public void testGetTipoInterés() {
+        System.out.println("getTipoInter\u00e9s");
+        CCuenta instance = new CCuenta();
+        double expResult = 0.0;
+        double result = instance.getTipoInterés();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setTipoInterés method, of class CCuenta.
+     */
+    @Test
+    public void testSetTipoInterés() {
+        System.out.println("setTipoInter\u00e9s");
+        double tipoInterés = 0.0;
+        CCuenta instance = new CCuenta();
+        instance.setTipoInterés(tipoInterés);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
